ee7fb37f39f73601ec0d786c8059d783a3e25465
Omer Ozyilmaz <omerrozyilmaz@gmail.com>
2022-04-30 22:50:27 +0300

    Minor update

--- nnObjs.py
+++ nnObjs.py
@@ -12,12 +12,24 @@ class layerDense:                               # https://www.youtube.com/watch?
         self.weights = 0.1*np.random.randn(numNeurons, numInputs) # it is desired for weights to have values between -1 and 1
         self.biases = np.zeros((numNeurons, 1)) # biases are used to prevent neurons from producing zeros
     def forward(self, inputs):
+        self.inputs = inputs
         self.output = np.dot(self.weights, inputs) + self.biases
+    def backward(self, dvalues):
+        # Gradients on biases
+        self.dbiases = sum(dvalues, axis = 1, keepdims = True)
+        # Gradients on weights
+        self.dweights = np.dot(self.inputs, dvalues.T)
+        self.dweights = self.dweights.T
+        # Gradients on inputs
+        self.dinputs = np.dot(self.weights.T, dvalues)
 
 class  actReLu:                                 # rectified linear activation function
-    def forward(self, inputs):                  # https://www.youtube.com/watch?v=gmjzbpSVY1A&list=PLQVvvaa0QuDcjD5BAw2DxE6OF2tius3V3&index=5
+    def forward(self, inputs):
+        self.inputs = inputs                  # https://www.youtube.com/watch?v=gmjzbpSVY1A&list=PLQVvvaa0QuDcjD5BAw2DxE6OF2tius3V3&index=5
         self.output = np.maximum(0, inputs)
-
+    def backward(self, dvalues):
+        self.dinputs = dvalues.copy()
+        self.dinputs[self.inputs <= 0] = 0
 class actSoftmax:                               # softmax activation function (exponentiation and normalization for returning correctness probability distribution fror each sample)
     def forward(self, inputs):                  # https://www.youtube.com/watch?v=omz_NdFgWyU&list=PLQVvvaa0QuDcjD5BAw2DxE6OF2tius3V3&index=6
          expValues = np.exp(inputs - np.max(inputs, axis=0, keepdims=True)) 
