6e80cd3acd0c7ea107140654375ff97f1ffc4e6b
Omer Ozyilmaz <omerrozyilmaz@gmail.com>
2022-05-01 19:29:20 +0300

    Implemented backward propogation for softmax and loss functions

--- nnApp.py
+++ nnApp.py
@@ -14,20 +14,70 @@ act1 = actReLu()
 layer2 = layerDense(3, 3)
 act2 = actSoftmax()
 
-# give input and take output
-layer1.forward(X)
-act1.forward(layer1.output)
+for i in range(1700):
+    # give input and take output
+    layer1.forward(X)
+    act1.forward(layer1.output)
 
-layer2.forward(act1.output)
-act2.forward(layer2.output)
+    layer2.forward(act1.output)
+    act2.forward(layer2.output)
 
-# calculate loss of outputs for given results
-lossFunc = lossCatCrossEnt()
-loss = lossFunc.calculate(act2.output, y)
+    # calculate loss of outputs for given results
+    lossFunc = lossCatCrossEnt()
+    loss = lossFunc.calculate(act2.output, y)
 
-# 
+    # backpropogate
+    lossFunc.backward(act2.output, y)
+    dvalues1 = lossFunc.dinputs
+
+    act2.backward(dvalues1)
+    dvalues2 = act2.dinputs
+
+    layer2.backward(dvalues2)
+    dweightslayer2 = layer2.dweights
+    dbiaseslayer2 = layer2.dbiases
+    dvalues3 = layer2.dinputs
+
+    act1.backward(dvalues3)
+    dvalues4 = act1.dinputs
+
+    layer1.backward(dvalues4)
+    dweightslayer1 = layer1.dweights
+    dbiaseslayer1 = layer1.dbiases
+
+    # update weights and biases
+    layer1.weights = np.subtract(layer1.weights, layer1.dweights)
+    layer1.biases = np.subtract(layer1.biases, layer1.biases)
+
+    layer2.weights = np.subtract(layer2.weights, layer2.dweights)
+    layer2.biases = np.subtract(layer2.biases, layer2.biases)
+
+    # give input and take output
+    layer1.forward(X)
+    act1.forward(layer1.output)
+
+    layer2.forward(act1.output)
+    act2.forward(layer2.output)
+
+    # calculate loss of outputs for given results
+    lossFunc = lossCatCrossEnt()
+    loss = lossFunc.calculate(act2.output, y)
+    print(str(loss) + "     " + str(i))
+#######################################################
+print("POST-PROCESS") 
 print("Loss:", loss)
-#output = act2.output
-#print(output[[0, 2], 0:7])
+
+print(y[0:5])
+print(act2.output[:, 0:5])
+
+print(dvalues1[:, 0:5])
+print(dvalues2[:, 0:5])
+print(dweightslayer2[:, 0:5])
+print(dbiaseslayer2[:, 0:5])
+print(dvalues3[:, 0:5])
+print(dvalues4[:, 0:5])
+print(dweightslayer1[:, 0:5])
+print(dbiaseslayer1[:, 0:5])
+
 plt.scatter(X[0], X[1])
 #plt.show()
--- nnObjs.py
+++ nnObjs.py
@@ -3,9 +3,9 @@ import nnfs
 from nnfs.datasets import spiral_data
 import matplotlib.pyplot as plt
 
-# CREATE CLASSES TO BE USED FOR DEFINING LAYER PROPERTIES (WEIGHT, BIAS, ACTIVATION FUNCTION, LOSS FUNCTION)
-
 nnfs.init()
+
+# CREATE CLASSES TO BE USED FOR DEFINING LAYER PROPERTIES (WEIGHT, BIAS, ACTIVATION FUNCTION, LOSS FUNCTION)
 
 class layerDense:                               # https://www.youtube.com/watch?v=TEWy9vZcxW4&list=PLQVvvaa0QuDcjD5BAw2DxE6OF2tius3V3&index=4
     def __init__(self, numNeurons, numInputs):  
@@ -16,7 +16,7 @@ class layerDense:                               # https://www.youtube.com/watch?
         self.output = np.dot(self.weights, inputs) + self.biases
     def backward(self, dvalues):
         # Gradients on biases
-        self.dbiases = sum(dvalues, axis = 1, keepdims = True)
+        self.dbiases = np.sum(dvalues, axis = 1, keepdims = True)
         # Gradients on weights
         self.dweights = np.dot(self.inputs, dvalues.T)
         self.dweights = self.dweights.T
@@ -30,15 +30,27 @@ class  actReLu:                                 # rectified linear activation fu
     def backward(self, dvalues):
         self.dinputs = dvalues.copy()
         self.dinputs[self.inputs <= 0] = 0
+
+class actLin:
+    def forward(self, inputs):
+        self.inputs = inputs
+        self.output = inputs
+    def backward(self, dvalues):
+        self.dinputs = dvalues.copy()
+
 class actSoftmax:                               # softmax activation function (exponentiation and normalization for returning correctness probability distribution fror each sample)
     def forward(self, inputs):                  # https://www.youtube.com/watch?v=omz_NdFgWyU&list=PLQVvvaa0QuDcjD5BAw2DxE6OF2tius3V3&index=6
          expValues = np.exp(inputs - np.max(inputs, axis=0, keepdims=True)) 
          probs = expValues / np.sum(expValues, axis=0, keepdims=True)   # sum through row (in other words, sum the column)
          self.output = probs
-
-class actLin:
-    def forward(self, inputs):
-        self.output = inputs
+    def backward(self, dvalues):
+        self.dinputs = np.empty_like(dvalues)
+        numSamples = self.output.shape[1]
+        for sampleIdx in range (numSamples):
+            singleOutput = self.output[:, [sampleIdx]]
+            singleDVlaues = dvalues[:, [sampleIdx]]
+            jacobian = np.diagflat(singleOutput) - np.dot(singleOutput, singleOutput.T)
+            self.dinputs[:, [sampleIdx]] = np.dot(jacobian.T, singleDVlaues)
 
 class lossCatCrossEnt:                                     
     def forward(self, yPred, yTrue):            #   categorical cross-entropy
@@ -53,5 +65,13 @@ class lossCatCrossEnt:
     def calculate(self, output, y):
         sampleLosses = self.forward(output, y)
         dataLoss = np.mean(sampleLosses)
-        return dataLoss                       
+        return dataLoss
+    def backward(self, dvalues, yTrue):
+        numSample = dvalues.shape[1]
+        numLabel = dvalues.shape[0]
+        if len(yTrue.shape) == 1:
+            yTrue = np.eye(numLabel)[:, yTrue]
+        self.dinputs = -yTrue/dvalues
+        self.dinputs = self.dinputs/numSample
+
 
